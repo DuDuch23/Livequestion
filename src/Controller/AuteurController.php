aabccc6ed27964b409698de746db0e7dff8aa661
Alexandre Duchemin <alexduduch77@gmail.com>
2024-08-31 00:57:25 +0200

    affichage de tout les users et création du profil utilisateur

--- src/Controller/AuteurController.php
+++ src/Controller/AuteurController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\ThematicRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AuteurController extends AbstractController
 {
     #[Route('/auteur', name: 'auteur')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, ThematicRepository $thematicRepository, Request $request): Response
     {
+        $users = $userRepository->findAll();
+
+        $thematics = $thematicRepository->findall();
+
+
         return $this->render('auteur/index.html.twig', [
-            'controller_name' => 'AuteurController',
+            'users' => $users,
+            'thematics' => $thematics
+        ]);
+    }
+
+    #[Route('/auteur/profil/{id}', name: 'profil_auteur')]
+    public function afficherProfil($id, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+
+        return $this->render('auteur/profil.html.twig', [
+            'user'=> $user,
         ]);
     }
 }
